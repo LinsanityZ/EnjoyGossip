11a3003ea6e2a292a381042bd53012f2aafe44d0
Function <15813251301@163.com>
2017-03-17 11:36:38 +0800

    修复bug

--- app/Root.js
+++ app/Root.js
@@ -9,7 +9,7 @@ import {createStore, applyMiddleware, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import * as reducers from './reducers';
-import App from './containers/App';
+import App from './containers/App0';
 
 
 /**
